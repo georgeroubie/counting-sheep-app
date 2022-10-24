7ee447f55d5f227bf2c3d111b0e5dbe80fe19963
George Roubie <george.roubie@gmail.com>
2022-10-25 00:43:19 +0300

    Update Theme.jsx

--- src/app/theme/Theme.jsx
+++ src/app/theme/Theme.jsx
@@ -1,19 +1,12 @@
 import PropTypes from 'prop-types';
-import { useContext } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
-import { AppContext } from '../state/Context';
 import GlobalStyles from './styles';
-import { darkTheme, DARK_THEME_KEY } from './themes/dark';
-import { lightTheme } from './themes/light';
+import baseTheme from './themes/base';
 
 const Theme = ({ children }) => {
-  const { state } = useContext(AppContext);
-  const { theme } = state;
-  const currentTheme = theme === DARK_THEME_KEY ? darkTheme : lightTheme;
-
   return (
-    <ThemeProvider theme={currentTheme}>
+    <ThemeProvider theme={baseTheme}>
       <Normalize />
       <GlobalStyles />
       {children}
