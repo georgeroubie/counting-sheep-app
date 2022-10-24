2bc98c9abd9ce0531295e89ade3bdf85aa1c1239
George Roubie <george.roubie@gmail.com>
2022-10-25 00:43:12 +0300

    Update reducer.js

--- src/app/state/reducer.js
+++ src/app/state/reducer.js
@@ -1,17 +1,5 @@
-import * as actionTypes from './actions';
-
 function appReducer(state, { type, value }) {
   switch (type) {
-    case actionTypes.UPDATE_USER_NAME:
-      return {
-        ...state,
-        userName: value,
-      };
-    case actionTypes.UPDATE_THEME_SELECTION:
-      return {
-        ...state,
-        theme: value,
-      };
     default:
       return state;
   }
