9e52d912929c494f6e6866b9261484eea875e947
George Roubie <george.roubie@gmail.com>
2022-10-26 13:48:52 +0300

    Update PageWrapper.jsx

--- src/app/components/layout/PageWrapper.jsx
+++ src/app/components/layout/PageWrapper.jsx
@@ -2,10 +2,9 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  text-align: center;
-  max-width: 800px;
   margin: ${({ theme: { spacing } }) => spacing.xlarge} auto;
-  padding: 0 ${({ theme: { spacing } }) => spacing.large};
+  padding: ${({ theme: { spacing, sizes } }) => `0 ${spacing.large} ${sizes.grassHeight} ${spacing.large}`};
+  overflow-y: auto;
 `;
 
 const PageWrapper = ({ children }) => {
