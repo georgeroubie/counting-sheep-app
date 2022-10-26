fedd4e5e978ec52b238dbdef533ff71bac57c44c
George Roubie <george.roubie@gmail.com>
2022-10-26 13:49:03 +0300

    Update Menu.jsx

--- src/app/routing/Menu.jsx
+++ src/app/routing/Menu.jsx
@@ -8,8 +8,8 @@ const Wrapper = styled.nav`
   width: 100%;
   position: fixed;
   bottom: 0;
-  background-color: #348c31;
-  height: 60px;
+  background: linear-gradient(180deg, rgba(2, 0, 36, 1) 0%, rgba(45, 208, 31, 1) 0%, rgba(18, 152, 14, 0.83) 100%);
+  height: ${({ theme: { sizes } }) => sizes.grassHeight};
 `;
 
 const NavLink = styled(_NavLink)`
