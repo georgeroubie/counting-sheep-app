e9c0d725ab2ec5991bee53caaaa3ab553050e773
George Roubie <george.roubie@gmail.com>
2022-10-27 13:51:44 +0300

    Update Title.jsx

--- src/app/components/typography/Title.jsx
+++ src/app/components/typography/Title.jsx
@@ -1,19 +1,43 @@
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import _BackIcon from '../icons/Back';
 
 const Wrapper = styled.h1`
   font-size: ${({ theme: { fontSize } }) => fontSize.xlarge};
   line-height: ${({ theme: { lineHeight } }) => lineHeight.xlarge};
   font-weight: ${({ theme: { fontWeight } }) => fontWeight.extraBold};
   margin: 0 0 ${({ theme: { spacing } }) => spacing.large};
+
+  ${({ $hasClick }) =>
+    $hasClick &&
+    css`
+      display: flex;
+      align-items: center;
+      gap: ${({ theme: { spacing } }) => spacing.normal};
+      cursor: pointer;
+    `}
 `;
 
-const Title = ({ children }) => {
-  return <Wrapper>{children}</Wrapper>;
+const BackIcon = styled(_BackIcon)`
+  width: ${({ theme: { fontSize } }) => fontSize.large};
+`;
+
+const Title = ({ children, onBack }) => {
+  return (
+    <Wrapper $hasClick={!!onBack} onClick={onBack}>
+      {onBack && <BackIcon />}
+      {children}
+    </Wrapper>
+  );
 };
 
 Title.propTypes = {
   children: PropTypes.node.isRequired,
+  onBack: PropTypes.func,
+};
+
+Title.defaultProps = {
+  onBack: undefined,
 };
 
 export default Title;
