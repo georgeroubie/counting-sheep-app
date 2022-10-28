09d081f93ce913d06e4c1154498e524a273b9e5d
George Roubie <george.roubie@gmail.com>
2022-10-28 13:22:17 +0300

    Update index.jsx

--- src/app/pages/about/index.jsx
+++ src/app/pages/about/index.jsx
@@ -7,6 +7,7 @@ import Title from '../../components/typography/Title';
 
 const Hyperlink = styled.a`
   text-decoration: underline;
+  display: inline-block;
 `;
 
 const About = () => {
@@ -21,25 +22,47 @@ const About = () => {
       <Title onBack={onBackHandler}>About</Title>
       <Subtitle>Info</Subtitle>
       <Description>
-        A counting sheep app that will help you fall asleep. The application is created by George Roubie (me). You can
-        find the code of this web application on my GitHub page. If you like it give the repository a star on&nbsp;
+        A counting sheep app that will help you fall asleep. You can count them with your mind, which is very effective,
+        or you can use the count button. Thank you for using it, good night!
+      </Description>
+      <Subtitle>Privacy</Subtitle>
+      <Description>Counting sheep web application does not use any kind of cookies or tracking.</Description>
+      <Subtitle>Code</Subtitle>
+      <Description>
+        You can find the code of this web application on my GitHub page. If you like it give the repository a star
+        on&nbsp;
         <Hyperlink href="https://github.com/georgeroubie/counting-sheep-app" target="_blank" rel="noreferrer">
           GitHub
         </Hyperlink>
-        &nbsp;and follow me on&nbsp;
+        . You can find the code of the night sky&nbsp;
+        <Hyperlink href="https://codepen.io/georgeroubie/pen/RwyYqWO" target="_blank" rel="noreferrer">
+          here
+        </Hyperlink>
+        &nbsp;and the code of the sheep&nbsp;
+        <Hyperlink href="https://codepen.io/georgeroubie/pen/abGxRXX" target="_blank" rel="noreferrer">
+          here
+        </Hyperlink>
+        . If you like them add a heart in each pen.
+      </Description>
+      <Subtitle>Creator</Subtitle>
+      <Description>
+        My name is George Roubie and you can follow me on&nbsp;
         <Hyperlink href="https://www.linkedin.com/in/georgeroubie" target="_blank" rel="noreferrer">
           LinkedIn
         </Hyperlink>
-        &nbsp;and&nbsp;
+        ,&nbsp;
         <Hyperlink href="https://george-roubie.medium.com" target="_blank" rel="noreferrer">
           Medium
         </Hyperlink>
-        . Thank you for using it, have fun!
-      </Description>
-      <Subtitle>Data and Privacy</Subtitle>
-      <Description>
-        Counting sheep web application does not use any kind of cookies or tracking. All the data are saved on your
-        device.
+        ,&nbsp;
+        <Hyperlink href="https://codepen.io/georgeroubie" target="_blank" rel="noreferrer">
+          Codepen
+        </Hyperlink>
+        &nbsp;and&nbsp;
+        <Hyperlink href="https://github.com/georgeroubie" target="_blank" rel="noreferrer">
+          GitHub
+        </Hyperlink>
+        .
       </Description>
     </PageWrapper>
   );
