8068fde6b80521e5087ae5f5c6adf8bffce9dbea
George Roubie <george.roubie@gmail.com>
2022-10-28 10:29:13 +0300

    Update PageWrapper.jsx

--- src/app/components/layout/PageWrapper.jsx
+++ src/app/components/layout/PageWrapper.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   margin: 0 auto;
-  padding: ${({ theme: { spacing } }) => `${spacing.xlarge} ${spacing.large} ${spacing.xlarge} ${spacing.large}`};
+  padding: ${({ theme: { spacing } }) => `${spacing.large}`};
   height: 100vh;
   overflow-y: auto;
   background-color: rgba(0, 0, 0, 0.7);
