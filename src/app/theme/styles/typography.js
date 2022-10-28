f8d271b1cc8d73cadeb4fa9afbc0b11dc75efd84
George Roubie <george.roubie@gmail.com>
2022-10-28 10:29:25 +0300

    Update typography.js

--- src/app/theme/styles/typography.js
+++ src/app/theme/styles/typography.js
@@ -2,7 +2,7 @@ import { css } from 'styled-components';
 
 const typography = css`
   body {
-    font-family: Arial, sans-serif;
+    font-family: 'Fredoka', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     font-size: ${({ theme: { fontSize } }) => fontSize.normal};
